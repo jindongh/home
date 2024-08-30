7f76d11a95048af4441ba8e1a0347555eeccdc5d
Jindong Huang <jindongh@gmail.com>
2024-08-29 22:18:19 -0700

    add template and home page

--- app.go
+++ app.go
@@ -1,33 +1,54 @@
 package main
 
 import (
+    "embed"
     "log"
+    "net/http"
     "os"
     "path/filepath"
 
 
     "github.com/joho/godotenv"
-    "github.com/gofiber/fiber/v3"
+    "github.com/gofiber/fiber/v2"
+    "github.com/gofiber/fiber/v2/middleware/filesystem"
+    "github.com/gofiber/template/html/v2"
 )
 
+//go:embed templates/*
+var viewsfs embed.FS
+//go:embed static/*
+var staticfs embed.FS
+
+type config struct {
+    port string
+    HomeUrl string
+    BookUrl string
+    DownloadUrl string
+    VideoUrl string
+    PhotoUrl string
+}
 func main() {
-    loadConfig()
-    port := os.Getenv("PORT")
+    config := loadConfig()
 
     // Initialize a new Fiber app
-    app := fiber.New()
-
-    // Define a route for the GET method on the root path '/'
-    app.Get("/", func(c fiber.Ctx) error {
-        // Send a string response to the client
-        return c.SendString("Hello, World 👋!")
+    app := fiber.New(fiber.Config{
+        Views: html.NewFileSystem(http.FS(viewsfs), ".html"),
+    })
+    app.Use("/static", filesystem.New(filesystem.Config{
+        Root: http.FS(staticfs),
+        PathPrefix: "static",
+    }))
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.Render("templates/index", fiber.Map{
+            "Config": config,
+        })
     })
 
     // Start the server on port 
-    log.Fatal(app.Listen("0.0.0.0:" + port))
+    log.Fatal(app.Listen("0.0.0.0:" + config.port))
 }
 
-func loadConfig() {
+func loadConfig() *config {
     ex, err := os.Executable()
     if err != nil {
         panic(err)
@@ -36,4 +57,12 @@ func loadConfig() {
     // Get the directory of the executable.
     dir := filepath.Dir(ex)
     godotenv.Load(dir + "/.env")
+    return &config{
+        port: os.Getenv("PORT"),
+        HomeUrl: os.Getenv("URL_HOME"),
+        VideoUrl: os.Getenv("URL_VIDEO"),
+        PhotoUrl: os.Getenv("URL_PHOTO"),
+        BookUrl: os.Getenv("URL_VIDEO"),
+        DownloadUrl: os.Getenv("URL_DOWNLOAD"),
+    }
 }
